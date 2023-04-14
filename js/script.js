6703f2425e21dddfde7b3a9f07ae6b234c52c8d0
AnthonyD <110226348+AnthonyDe12@users.noreply.github.com>
2023-04-13 19:59:17 -0500

    Init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,97 @@
-function revealMessage(){
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+new Chart(ctx,config);
+
+
+  function revealMessage(){
     document.getElementById("hiddenMessage").style.display = 'block';
 }
+
+fetch('http//:localhost:8080/bank_data')
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+    const ctf = document.getElementById('myChart');
+
+new Chart(ctf, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+new Chart(ctm,config);
+
+
+  function revealMessage(){
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+fetch('http//:localhost:8080/bank_data')
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+    const ctm = document.getElementById('myChart');
+
+new Chart(ctm, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+new Chart(ctm,config);
+
+
+  function revealMessage(){
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+fetch('http//:localhost:8080/bank_data')
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+    
---
+++ js/script2.js
@@ -0,0 +1,35 @@
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+new Chart(ctx,config);
+
+
+  function revealMessage(){
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+fetch('http//:localhost:8080/bank_data')
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+    const ctf = document.getElementById('myChart');
+
